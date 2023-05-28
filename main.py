a1f88d38fdea508896b78940098461075c870092
hammi <104840585+abouhoraira2006@users.noreply.github.com>
2023-05-28 10:49:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,122 +1,66 @@
-import math
-import aiosqlite
-import asyncio
 import discord
 from discord.ext import commands
+import sqlite3
 
-# should be fixed
+conn = sqlite3.connect('levels.db')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS levels
+             (user_id TEXT PRIMARY KEY, level INTEGER, exp INTEGER)''')
 
 intents = discord.Intents.default()
-intents.members = True
-bot = commands.Bot(command_prefix="!", intents=intents)
-bot.multiplier = 1
+intents.typing = False
+intents.presences = False
 
-async def initialize():
-    await bot.wait_until_ready()
-    bot.db = await aiosqlite.connect("expData.db")
-    await bot.db.execute("CREATE TABLE IF NOT EXISTS guildData (guild_id int, user_id int, exp int, PRIMARY KEY (guild_id, user_id))")
-    
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
 @bot.event
 async def on_ready():
-    print(bot.user.name + " is ready.")
-@bot.command()
-async def ping(ctx, arg=None, option=1):
-    if arg == "pong":
-        await ctx.channel.send("You've already ponged yourself!")
+    print(f'Logged in as {bot.user.name}')
+
 
 @bot.event
 async def on_message(message):
-    if not message.author.bot:
-        cursor = await bot.db.execute("INSERT OR IGNORE INTO guildData (guild_id, user_id, exp) VALUES (?,?,?)", (message.guild.id, message.author.id, 1)) 
+    if message.author.bot:
+        return
 
-        if cursor.rowcount == 0:
-            await bot.db.execute("UPDATE guildData SET exp = exp + 1 WHERE guild_id = ? AND user_id = ?", (message.guild.id, message.author.id))
-            cur = await bot.db.execute("SELECT exp FROM guildData WHERE guild_id = ? AND user_id = ?", (message.guild.id, message.author.id))
-            data = await cur.fetchone()
-            exp = data[0]
-            lvl = math.sqrt(exp) / bot.multiplier
-        
-            if lvl.is_integer():
-                await message.channel.send(f"{message.author.mention} well done! You're now level: {int(lvl)}.")
+    user_id = str(message.author.id)
+    c.execute('SELECT * FROM levels WHERE user_id=?', (user_id,))
+    result = c.fetchone()
 
-        await bot.db.commit()
+    if result is None:
+        c.execute('INSERT INTO levels VALUES (?, ?, ?)', (user_id, 0, 0))
+    else:
+        exp = result[2] + 1
+        level = result[1]
+        if exp >= level * 100:
+            exp = 0
+            level += 1
+            await message.channel.send(f'{message.author.mention} مستواك الجديد هو {level}!')
+        c.execute('UPDATE levels SET level=?, exp=? WHERE user_id=?', (level, exp, user_id))
 
+    conn.commit()
     await bot.process_commands(message)
 
-@bot.command()
-async def stats(ctx, member: discord.Member=None):
-    if member is None: member = ctx.author
-
-    # get user exp
-    async with bot.db.execute("SELECT exp FROM guildData WHERE guild_id = ? AND user_id = ?", (ctx.guild.id, member.id)) as cursor:
-        data = await cursor.fetchone()
-        exp = data[0]
-
-        # calculate rank
-    async with bot.db.execute("SELECT exp FROM guildData WHERE guild_id = ?", (ctx.guild.id,)) as cursor:
-        rank = 1
-        async for value in cursor:
-            if exp < value[0]:
-                rank += 1
-
-    lvl = int(math.sqrt(exp)//bot.multiplier)
-
-    current_lvl_exp = (bot.multiplier*(lvl))**2
-    next_lvl_exp = (bot.multiplier*((lvl+1)))**2
-
-    lvl_percentage = ((exp-current_lvl_exp) / (next_lvl_exp-current_lvl_exp)) * 100
-
-    embed = discord.Embed(title=f"Stats for {member.name}", colour=discord.Colour.gold())
-    embed.add_field(name="Level", value=str(lvl))
-    embed.add_field(name="Exp", value=f"{exp}/{next_lvl_exp}")
-    embed.add_field(name="Rank", value=f"{rank}/{ctx.guild.member_count}")
-    embed.add_field(name="Level Progress", value=f"{round(lvl_percentage, 2)}%")
-
-    await ctx.send(embed=embed)
 
 @bot.command()
-async def leaderboard(ctx): 
-    buttons = {}
-    for i in range(1, 6):
-        buttons[f"{i}\N{COMBINING ENCLOSING KEYCAP}"] = i # only show first 5 pages
+async def stats(ctx):
+    user_id = str(ctx.author.id)
+    c.execute('SELECT * FROM levels WHERE user_id=?', (user_id,))
+    result = c.fetchone()
 
-    previous_page = 0
-    current = 1
-    index = 1
-    entries_per_page = 10
-
-    embed = discord.Embed(title=f"Leaderboard Page {current}", description="", colour=discord.Colour.gold())
-    msg = await ctx.send(embed=embed)
-
-    for button in buttons:
-        await msg.add_reaction(button)
-
-    while True:
-        if current != previous_page:
-            embed.title = f"Leaderboard Page {current}"
-            embed.description = ""
-
-            async with bot.db.execute(f"SELECT user_id, exp FROM guildData WHERE guild_id = ? ORDER BY exp DESC LIMIT ? OFFSET ? ", (ctx.guild.id, entries_per_page, entries_per_page*(current-1),)) as cursor:
-                index = entries_per_page*(current-1)
-
-                async for entry in cursor:
-                    index += 1
-                    member_id, exp = entry
-                    member = ctx.guild.get_member(member_id)
-                    embed.description += f"{index}) {member.mention} : {exp}\n"
-
-                await msg.edit(embed=embed)
-
-        try:
-            reaction, user = await bot.wait_for("reaction_add", check=lambda reaction, user: user == ctx.author and reaction.emoji in buttons, timeout=60.0)
-
-        except asyncio.TimeoutError:
-            return await msg.clear_reactions()
-
-        else:
-            previous_page = current
-            await msg.remove_reaction(reaction.emoji, ctx.author)
-            current = buttons[reaction.emoji]
+    if result is None:
+        embed=discord.Embed(title="Levels", description="Show levels", color=0xdd0239)
+        embed.add_field(name="Level", value="You hav not level now", inline=False)
+        await ctx.send(embed=embed)
+    else:
+        level = result[1]
+        exp = result[2]
+        embed=discord.Embed(title="Levels", description="Show levels", color=0x0070df)
+        embed.add_field(name="Level", value="*****{level}*", inline=False)
+        embed.add_field(name="Xp", value="***{exp}***", inline=True)
+        await ctx.send(embed=embed)
 
 @bot.remove_command("help")
 @bot.command()
@@ -129,10 +73,8 @@ async def help(ctx):
     embed.set_footer(text="beata version")
     await ctx.send(embed=embed)
 
-bot.loop.create_task(initialize())
 To = 'OTU2MTgyMTM5OTk1NTUzODQy'
 ke = '.G_19bR.umqMjRn_'
 ns = 'qiMlxUe9QDNPp0kaSGKPbeoOWllQlE'
 Token = To + ke + ns
 bot.run(Token)
-asyncio.run(bot.db.close())
